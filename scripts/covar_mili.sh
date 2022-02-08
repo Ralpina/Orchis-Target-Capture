bcf274b1a939b89f801d93ae67700bde91057802
Ralpina <48416466+Ralpina@users.noreply.github.com>
2022-02-08 14:09:37 +0000

    Add files via upload

---
+++ scripts/covar_mili.sh
@@ -0,0 +1,4 @@
+gatk AnalyzeCovariates \
+  -before ./results/bqsr/recal.mili.table \
+  -after ./results/bqsr/recal2.mili.table \
+  -plots ./results/bqsr/AnalyzeCovariates_round1_mili.pdf
---
+++ scripts/covar_purp.sh
@@ -0,0 +1,4 @@
+gatk AnalyzeCovariates \
+  -before ./results/bqsr/recal.purp.table \
+  -after ./results/bqsr/recal2.purp.table \
+  -plots ./results/bqsr/AnalyzeCovariates_round1_purp.pdf
