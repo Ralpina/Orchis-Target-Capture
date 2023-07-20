e75661946efe64c63c9a0ae6ea2f9cbbe6417c66
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-07-20 15:11:09 +0100

    Update sortfix.sh

--- scripts/sortfix.sh
+++ scripts/sortfix.sh
@@ -1,4 +1,9 @@
-for sample in `cat samplelist.txt`
-  do samtools fixmate -m ./results/mapping/militaris/${sample}.bam ./results/mapping/militaris/${sample}.fix.bam
-     samtools sort -o ./results/mapping/militaris/${sample}.sorted.bam ./results/mapping/militaris/${sample}.fix.bam
+for sample in `cat samplelist.txt`; do 
+ samtools fixmate -m ./results/mapping/militaris/${sample}.bam ./results/mapping/militaris/${sample}.fix.bam
+ samtools sort -o ./results/mapping/militaris/${sample}.sorted.bam ./results/mapping/militaris/${sample}.fix.bam
+done
+
+for sample in `cat samplelist.txt`; do 
+ samtools fixmate -m ./results/mapping/purpurea/${sample}.bam ./results/mapping/purpurea/${sample}.fix.bam
+ samtools sort -o ./results/mapping/purpurea/${sample}.sorted.bam ./results/mapping/purpurea/${sample}.fix.bam
 done
