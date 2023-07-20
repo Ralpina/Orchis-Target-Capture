a6c13ee22e05ff8d501a5ef16d5591a6b295e9d0
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-07-20 15:07:41 +0100

    Update and rename mapping to mapping.sh

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -4,3 +4,11 @@ for sample in `cat samplelist.txt`
    ./data/${sample}_1.fastq \
    ./data/${sample}_2.fastq | samtools view -b -h -S > results/mapping/militaris/${sample}.bam
 done
+
+for sample in `cat samplelist.txt`
+  do bwa mem -M -t 10 \
+   ./results/hybpiper/refs/purpurea \
+   ./data/${sample}_1.fastq \
+   ./data/${sample}_2.fastq | samtools view -b -h -S > results/mapping/purpurea/${sample}.bam
+done
+
