56ec900cb546f68320dcaa0c0eccac09ab3c692c
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-07-20 15:19:12 +0100

    Update addRG.sh

--- scripts/addRG.sh
+++ scripts/addRG.sh
@@ -1,5 +1,5 @@
-for sample in `cat samplelist.txt`
-  do RUN_ID=Leif
+for sample in `cat samplelist.txt`; do 
+    RUN_ID=Leif
     LANE_NUM=1
 	RGID_specify=${RUN_ID}_${LANE_NUM}
 	RGLB_specify=${sample}
@@ -8,6 +8,27 @@ for sample in `cat samplelist.txt`
 	RGSM_specify=${sample}
     gatk AddOrReplaceReadGroups --INPUT ./results/mapping/militaris/${sample}.unique.bam \
 	--OUTPUT ./results/mapping/militaris/${sample}.uniqueRG.bam \
+	--SORT_ORDER coordinate \
+	--RGID ${RGID_specify} \
+	--RGLB ${RGLB_specify} \
+	--RGPL ${RGPL_specify} \
+	--RGPU ${RGPU_specify} \
+	--RGSM ${RGSM_specify} \
+	--CREATE_INDEX True \
+	--VALIDATION_STRINGENCY LENIENT
+done
+
+
+for sample in `cat samplelist.txt`; do
+    RUN_ID=Leif
+    LANE_NUM=1
+	RGID_specify=${RUN_ID}_${LANE_NUM}
+	RGLB_specify=${sample}
+	RGPL_specify=ILLUMINA
+	RGPU_specify=${RUN_ID}_${LANE_NUM}
+	RGSM_specify=${sample}
+    gatk AddOrReplaceReadGroups --INPUT ./results/mapping/purpurea/${sample}.unique.bam \
+	--OUTPUT ./results/mapping/purpurea/${sample}.uniqueRG.bam \
 	--SORT_ORDER coordinate \
 	--RGID ${RGID_specify} \
 	--RGLB ${RGLB_specify} \
