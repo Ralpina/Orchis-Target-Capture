f304ead98798fe3eacc800e9116c712d81f9d636
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-07-21 17:13:39 +0100

    Update repBQSR.sh

--- scripts/repBQSR.sh
+++ scripts/repBQSR.sh
@@ -3,3 +3,9 @@ gatk BaseRecalibrator \
 -R ./results/hybpiper/refs/militaris.fa \
 --known-sites ./results/known/militaris/calls.filter.vcf \
 -O ./results/bqsr/recal2.mili.table
+
+gatk BaseRecalibrator \
+-I ./results/bqsr/purp_recal.bam \
+-R ./results/hybpiper/refs/purpurea.fa \
+--known-sites ./results/known/purpurea/calls.filter.vcf \
+-O ./results/bqsr/recal2.purp.table 
