53277c372ef9e9450d5049d068608a210c0b4c68
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-08-09 17:04:09 +0100

    Update select_SNPs.sh

--- scripts/select_SNPs.sh
+++ scripts/select_SNPs.sh
@@ -3,3 +3,9 @@ gatk SelectVariants \
 	-O ./results/filtered_genotypes/ref_militaris_SNPs.vcf \
 	-V ./results/raw_genotypes/militaris_unfiltered.vcf.gz \
 	--select-type-to-include SNP
+
+gatk SelectVariants \
+	-R ./results/hybpiper/refs/purpurea.fa \
+	-O ./results/filtered_genotypes/ref_purpurea_SNPs.vcf \
+	-V ./results/raw_genotypes/purpurea_unfiltered.vcf.gz \
+	--select-type-to-include SNP
