bd159586e0f226d43ae66ffdc9952576e458f156
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-08-09 17:03:18 +0100

    Update and rename genotype_mili.sh to genotype.sh

--- scripts/genotype.sh
+++ scripts/genotype.sh
@@ -3,3 +3,9 @@ gatk GenotypeGVCFs \
    -V  gendb://results/called_genotypes/mili_database \
    --include-non-variant-sites \
    -O ./results/raw_genotypes/militaris_unfiltered.vcf.gz
+
+gatk GenotypeGVCFs \
+   -R ./results/hybpiper/refs/purpurea.fa \
+   -V  gendb://results/called_genotypes/purp_database \
+   --include-non-variant-sites \
+   -O ./results/raw_genotypes/purpurea_unfiltered.vcf.gz
