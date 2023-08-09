5eba4342267205735a2a358cba65e5cd8b5f0c96
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-08-09 16:58:14 +0100

    Update haploCall_mili.sh

--- scripts/haploCall_mili.sh
+++ scripts/haploCall_mili.sh
@@ -6,3 +6,12 @@ for sample in `cat samplelist.txt`
   --emit-ref-confidence GVCF \
   -O ./results/raw_genotypes/ref_mili/${sample}.raw.snps.indels.g.vcf
 done
+
+for sample in `cat samplelist.txt`
+  do gatk HaplotypeCaller \
+  -I ./results/bqsr/purp_recal2.bam \
+  --sample-name ${sample} \
+  -R ./results/hybpiper/refs/purpurea.fa \
+  --emit-ref-confidence GVCF \
+  -O ./results/raw_genotypes/ref_purp/${sample}.raw.snps.indels.g.vcf
+done
