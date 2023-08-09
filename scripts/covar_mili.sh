19b6986aba5653ee4ad2e719dc107fbed5d5249d
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-08-09 17:09:03 +0100

    Update covar.sh

--- scripts/covar_mili.sh
+++ scripts/covar_mili.sh
@@ -2,3 +2,8 @@ gatk AnalyzeCovariates \
   -before ./results/bqsr/recal.mili.table \
   -after ./results/bqsr/recal2.mili.table \
   -plots ./results/bqsr/AnalyzeCovariates_round1_mili.pdf
+
+gatk AnalyzeCovariates \
+  -before ./results/bqsr/recal.purp.table \
+  -after ./results/bqsr/recal2.purp.table \
+  -plots ./results/bqsr/AnalyzeCovariates_round1_purp.pdf
