27e5260009889b220e2cb421953c390583c42838
Roberta Gargiulo <48416466+Ralpina@users.noreply.github.com>
2023-08-09 17:07:45 +0100

    Delete genotype_purp.sh

--- scripts/genotype_purp.sh
+++
@@ -1,5 +0,0 @@
-gatk GenotypeGVCFs \
-   -R ./results/hybpiper/refs/purpurea.fa \
-   -V  gendb://results/called_genotypes/purp_database \
-   --include-non-variant-sites \
-   -O ./results/raw_genotypes/purpurea_unfiltered.vcf.gz
